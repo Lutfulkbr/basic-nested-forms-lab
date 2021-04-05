372b673abb6b53f6d4895aa918d519f02a1bbc8a
github username <github email address>
2021-04-04 21:56:29 -0400

    commit tree 85abaf366ebbeed986904799025a0bbd8e09ce4b

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,21 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    2.times {@recipe.ingredients.build}
   end
 
   def create
+    if recipe = Recipe.create(recipe_params)
+      redirect_to recipe_path(recipe)
+    else
+      render :new
+    end
   end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, ingredients_attributes: [:name, :quantity])
+  end
+
 end
